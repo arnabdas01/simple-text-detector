99c13e14b56829275964d9b4af95d1f3357b9192
arnabdas01 <dasarnab1800@gmail.com>
2021-10-19 11:10:25 +0530

    Few changes

--- Text_detection.py
+++ Text_detection.py
@@ -8,14 +8,14 @@ img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)              #as tesseract only under
 #Detecting Words
 img_h, img_w, _ = img.shape
 boxes = pyt.image_to_data(img)
-print(boxes)
+# print(boxes)
 # The structure of boxes is like:
 # [   0          1           2           3           4          5         6       7       8        9        10       11 ]
 # ['level', 'page_num', 'block_num', 'par_num', 'line_num', 'word_num', 'left', 'top', 'width', 'height', 'conf', 'text']
 for i, a in enumerate(boxes.splitlines()):
     if i != 0:
         a = a.split()
-        print(a)
+        # print(a)
         if len(a) == 12:
             x, y, w, h = int(a[6]), int(a[7]), int(a[8]), int(a[9])
             cv2.rectangle(img, (x, y), (w + x, h + y), (0, 0, 255), 2)              #rectangle around words
